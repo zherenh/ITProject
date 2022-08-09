293419818640c539628e0004675fa3ba9091337a
zherenh <zherenh@student.unimelb.edu.au>
2022-08-09 17:58:43 +1000

    Frame set uo

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom"
+import Layout from './components/Layout/Layout';
+import HomePage from './pages/HomePage';
 
 function App() {
-  return (
-    <div className="App">
-      React App
-    </div>
-  );
+	return (
+		<div className="App">
+			<Layout>
+				<Routes>
+					<Route path={"/"} element={<HomePage></HomePage>}></Route>
+				</Routes>
+			</Layout>
+		</div>
+	);
 }
 
 export default App;
---
+++ project/src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            内容主页
+        </div>
+    )
+}
+
+export default Home
