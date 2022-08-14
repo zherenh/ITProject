3652cd13b8cab2efffec25f15d25a513537607b4
zherenh <zherenh@student.unimelb.edu.au>
2022-08-15 06:05:39 +1000

    HomePage demo

--- project/src/components/Home/Home.js
+++ project/src/components/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import classes from './Home.module.css'
 
 const Home = () => {
     return (
-        <div>
-            内容主页
+        <div className={classes.Home}>
+            <p className={classes.Title}>LabelsHouse</p>
+            <div className={classes.Describe}>This Website is used to store the labels of link whatever legal or not.</div>
+            <button className={classes.Start}>Start</button>
+            <div className={classes.Auth}>
+                <button>Login</button>
+                <button>Register</button>
+            </div>
+            <div className={classes.Background}></div>
         </div>
     )
 }
