6a3051587e9aa1afd9d562a07dd71993673e25e1
zherenh <zherenh@student.unimelb.edu.au>
2022-08-15 15:38:30 +1000

    Title change

--- project/src/components/Home/Home.js
+++ project/src/components/Home/Home.js
@@ -4,7 +4,7 @@ import classes from './Home.module.css'
 const Home = () => {
     return (
         <div className={classes.Home}>
-            <p className={classes.Title}>LabelsHouse</p>
+            <p className={classes.Title}>Labels<p>House</p></p>
             <div className={classes.Describe}>This Website is used to store the labels of link whatever legal or not, and opened to anyone.</div>
             <button className={classes.Start}>Start</button>
             <div className={classes.Auth}>
