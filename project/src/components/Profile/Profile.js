dc561af0562ed5afed2c2b17c1c9f443cacfc7af
zherenh <zherenh@student.unimelb.edu.au>
2022-08-27 20:23:41 +1000

    Work from Julie

--- project/src/components/Profile/Profile.js
+++ project/src/components/Profile/Profile.js
@@ -1,12 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import TopBar from '../TopBar/TopBar'
 import classes from './Profile.module.css'
 
 const Profile = () => {
     return (
-        <div className={classes.Profile}>
+        <div className={classes.Home}>
             <TopBar></TopBar>
-            Profile
+            <div className={classes.Describe}></div>
+            {/* <div className={classes.Background}></div> */}
+            <div className={classes.mybox}>
+                <div className={classes.title}>
+                    Website Register
+                </div>
+                <div className={classes.ipt_box}>
+                    <input type="text" placeholder=' Customized Name'/>
+                </div>
+                <div className={classes.ipt_box}>
+                    <input type="text" placeholder=' Link'/>
+                </div>
+                <div className={classes.ipt_box}>
+                    <input type="text" placeholder=' Description'/>
+                </div>
+                <div className={classes.ipt_box}>
+                    <input type="text" placeholder=' Label'/>
+                </div>
+                <div className={classes.ipt_box}>
+                    <input type="text" placeholder=' Registered Time'/>
+                </div>
+                <div className={classes.ipt_box}>
+                    <input type="text" placeholder=' Imagination'/>
+                </div>
+                <button className={classes.submit}>
+                    Add
+                </button>
+            </div>
         </div>
     )
 }
