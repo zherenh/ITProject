b40f5add0ea64f7e2dba5b9bf3fba69ff41ab83a
yijuzhou <yijuzhou@student.unimelb.edu.au>
2022-09-12 20:19:55 +1000

    change modify page, still working

--- project/src/components/Modify/Modify.js
+++ project/src/components/Modify/Modify.js
@@ -10,81 +10,31 @@ const Modify = () => {
             <div className={classes.Stone2}></div>
             <div className={classes.Stone3}></div>
             <div className={classes.linkContainer}>
+
                 {/*link1*/}
                 <div className={classes.linkBox}>
                     <div className={classes.link}>
                         www.link1.com.au
-                    </div>
-                    <div className={classes.label}>
-                        label1
-                    </div>
-                    <div className={classes.label}>
-                        label2
-                    </div>
-                    <div className={classes.label}>
-                        label3
-                    </div>
-                </div>
-                {/*link2*/}
-                <div className={classes.linkBox}>
-                    <div className={classes.link}>
-                        www.link2.com.au
-                    </div>
-                    <div className={classes.label}>
-                        label1
-                    </div>
-                    <div className={classes.label}>
-                        label2
-                    </div>
-                    <div className={classes.label}>
-                        label3
-                    </div>
-                </div>
-                {/*link3*/}
-                <div className={classes.linkBox}>
-                    <div className={classes.link}>
-                        www.link3.com.au
-                    </div>
-                    <div className={classes.label}>
-                        label1
-                    </div>
-                    <div className={classes.label}>
-                        label2
-                    </div>
-                    <div className={classes.label}>
-                        label3
-                    </div>
-                </div>
-                {/*link4*/}
-                <div className={classes.linkBox}>
-                    <div className={classes.link}>
-                        www.link4.com.au
-                    </div>
-                    <div className={classes.label}>
-                        label1
-                    </div>
-                    <div className={classes.label}>
-                        label2
-                    </div>
-                    <div className={classes.label}>
-                        label3
-                    </div>
-                </div>
 
-                {/* buttons bar*/}
-                <div className={classes.buttonContainer}>
-                    <button className={classes.editButton}>
+                        <div className={classes.label}>
+                            label1
+                        </div>
+                        <div className={classes.label}>
+                            label2
+                        </div>
+                        <div className={classes.label}>
+                            label3
+                        </div>
+                    </div>
+
+                    <div className={classes.deleteButton}> 
+                        -
+                    </div>
+                    <div className={classes.editButton}> 
                         Edit
-                    </button>
-                    <div>
-                        <button className={classes.addButton}>
-                            Add
-                        </button>
-                        <button className={classes.doneButton}>
-                            Done
-                        </button>
                     </div>
                 </div>
+                
             </div>
         </div>
     )
